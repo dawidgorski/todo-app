4631631995c3034f53c23ccc106adfd147c5fe20
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-05-31 17:18:18 +0200

    Add TestConfiguration

---
+++ src/test/java/com/dawidgorski/todoapp/model/TestConfiguration.java
@@ -0,0 +1,81 @@
+package com.dawidgorski.todoapp.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.util.*;
+
+@Configuration
+class TestConfiguration {
+    @Bean
+    @Primary
+    @Profile("!integration")
+    DataSource e2eTestDataSource() {
+        var result = new DriverManagerDataSource("jdbc:h2:mem:test;Db_CLOSE_DELAY=-1", "", "");
+        result.setDriverClassName("org.h2.Driver");
+        return result;
+    }
+
+    @Bean
+    @Primary
+    @Profile("integration")
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+            private Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public Task save(final Task entity) {
+                int key = tasks.size() + 1;
+
+                try {
+                   var field = Task.class.getDeclaredField("id");
+                    field.setAccessible(true);
+                    field.set(entity, key);
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                tasks.put(key, entity);
+                return tasks.get(key);
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+        };
+    }
+}
