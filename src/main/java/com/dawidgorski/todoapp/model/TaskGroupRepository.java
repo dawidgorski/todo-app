fe226a4f5e1cbc7dbf45c660bf52bfe790815ce1
Dawid Górski <gorski.dawid.mail@gmail.com>
2021-06-04 15:04:38 +0200

    Remove findAll

--- src/main/java/com/dawidgorski/todoapp/model/TaskGroupRepository.java
+++ src/main/java/com/dawidgorski/todoapp/model/TaskGroupRepository.java
@@ -1,16 +1,15 @@
 package com.dawidgorski.todoapp.model;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskGroupRepository {
     List<TaskGroup> findAll();
-    Optional<TaskGroup> findById(Integer id);
-    TaskGroup save(TaskGroup entity) ;
-    Page<TaskGroup> findAll(Pageable page);
-    boolean existsByDoneIsFalseAndProject_Id(Integer groupId);
 
+    Optional<TaskGroup> findById(Integer id);
+
+    TaskGroup save(TaskGroup entity);
+
+    boolean existsByDoneIsFalseAndProject_Id(Integer projectId);
 }
